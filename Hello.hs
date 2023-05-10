8b9da18fa4ba039f129cb7cffa477679532fc1c6
yuyagishita <yu.yagishita1995@gmail.com>
2023-05-10 23:59:39 +0900

    feat(入門): 入門

--- Hello.hs
+++ Hello.hs
@@ -1,2 +1,32 @@
-main = do { putStrLn "Hello world!"; print (product[1..5]) }
+main = do print (add 2 4)
+          print (add 2 4 * 3)
+          print (square (add 2 (2 * 4)))
+          print (square $ add 2 $ 2 * 4)
+
+add x y = x + y
+square x = x * x
+
+n :: Int = 124
+m = 111 :: Int
+
+(a, b, c) = (123, 3.2, "hello")
+t@(d,e,f) = (123, 3.1, "wwwww")
+
+getValue defval maybe =
+  case maybe of
+    Nothing -> defval
+    Just x -> x
+
+absMaybe x =
+  case x of
+    Nothing -> 0
+    Just x | x < 0 -> -x
+           | otherwise -> x
+
+absss x = if x < 0 then -x else x
+
+area2 r = pi * square r
+  where
+    pi = 3.14
+    square x = x*x
 
