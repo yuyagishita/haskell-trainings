5300a493b0d407b92722d8552439e1b3d06f140f
yuyagishita <yu.yagishita1995@gmail.com>
2024-12-25 23:23:48 +0900

    oK

--- Monads.hs
+++ Monads.hs
@@ -12,6 +12,18 @@ div8 x = do y <- div2 x
             z <- div2 y
             div2 z
 
+div8NoSugar :: Int -> Maybe Int
+div8NoSugar x = 
+  case return x of
+    Nothing -> Nothing
+    Just y ->
+      case div2 y of
+        Nothing -> Nothing
+        Just z -> 
+          case div2 z of
+            Nothing -> Nothing
+            Just w -> div2 w
+
 main = do print $ fmap f (Just 5)
           print $ fmap f Nothing
           print $ fmap f [1,2,3]
@@ -24,3 +36,8 @@ main = do print $ fmap f (Just 5)
           print $ div8 32
           print $ div8 50
           print $ div8 256
+
+          print $ Just 32 >>= div8
+
+          print $ div8NoSugar 32
+
